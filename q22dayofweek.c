646446858c9ee77a37f7d094f6c8ef2e65c5aee8
Moushik R <moushikmoushik55@gmail.com>
2026-01-27 00:32:31 +0530

    Update q22dayofweek.c

---
+++ q22dayofweek.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    int day;
+    printf("enter the day number");
+    scanf("%d",&day);
+    switch(day){
+    case 1:
+    printf("sunday");
+    break;
+    case 2:
+    printf("Monday");
+    break;
+    case 3:
+    printf("Tuesday");
+    break;
+    case 4:
+    printf("wednesday");
+    break;
+    case 5:
+    printf("thursday");
+    break;
+    case 6:
+    printf("friday");
+    break;
+    case 7:
+    printf("saturday");
+    break;
+    }   
+    
+return 0;
+}
