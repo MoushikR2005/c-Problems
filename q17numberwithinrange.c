88a8649f8b1038339016fb4a61c102a8e9958e7a
Moushik R <moushikmoushik55@gmail.com>
2026-01-27 00:29:58 +0530

    Update q17numberwithinrange.c

---
+++ q17numberwithinrange.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=15;
+    if(a>=10&&a<=20){
+        printf("Number is within range");
+    }else{
+        printf("Number is without range");
+    }
+    return 0;
+}
