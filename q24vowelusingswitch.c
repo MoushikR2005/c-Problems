4e4e4668b804dca5ee3a683016685add29398ba8
Moushik R <moushikmoushik55@gmail.com>
2026-01-27 00:37:02 +0530

    Update and rename q24vowelusingswich.c to q24vowelusingswitch.c

---
+++ q24vowelusingswitch.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main(){
+   char ch;
+   printf("Enter the character:");
+   scanf("%c",&ch);
+   switch(ch){
+       case 'A':
+       printf("Vowels");
+       break;
+       case 'E':
+       printf("Vowels");
+       break;
+       case 'I':
+       printf("Vowels");
+       break;
+       case 'O':
+       printf("Vowels");
+       break;
+       case 'U':
+       printf("Vowels");
+       break;
+       case 'a':
+       printf("Vowels");
+       break;
+       case 'e':
+       printf("Vowels");
+       break;
+       case 'i':
+       printf("Vowels");
+       break;
+       case 'o':
+       printf("Vowels");
+       break;
+       case 'u':
+       printf("Vowels");
+       break;
+       
+       default:
+       printf("not a vowels");
+   }
+    return 0;
+}
