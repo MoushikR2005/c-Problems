aae7e241f4a596d2dda9db49e51c7a7514fda776
Moushik R <moushikmoushik55@gmail.com>
2026-01-28 00:23:44 +0530

    Add files via upload

---
+++ switch27.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    switch (n)
+    {
+    case 1:
+        printf("Hostel Fee = 80,000");
+        break;
+    case 2:
+        printf("Hostel Fee = 60,000");
+        break;
+    case 3:
+        printf("Hostel Fee = 45,000");
+        break;
+
+    default:
+        break;
+    }
+    return 0;
+}
