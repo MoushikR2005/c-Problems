49ed21447ccbc38d9e6b1e4c57f8ad4d5ee5df98
Moushik R <moushikmoushik55@gmail.com>
2026-01-28 00:30:58 +0530

    Add files via upload

---
+++ switch35.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    switch (n)
+    {
+    case 1:
+        printf("Upgrade Fee 300 ");
+        break;
+    case 2:
+        printf("Upgrade Fee 800");
+        break;
+    case 3:
+        printf("Upgrade Fee 1500");
+        break;
+
+    default:
+    printf("invaild");
+        break;
+    }
+
+    return 0;
+}
