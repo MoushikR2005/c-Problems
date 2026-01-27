7a24ddbdd53212246ebe769c5339700e7cac3906
Moushik R <moushikmoushik55@gmail.com>
2026-01-28 00:22:31 +0530

    Add files via upload

---
+++ switch24.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (){
+
+    int n;
+    scanf("%d",&n);
+
+    switch (n)
+    {
+    case 1:
+        printf("₹1200");
+        break;
+    case 2:
+        printf("₹800");
+        break;
+    case 3:
+        printf("₹500");
+        break;
+    
+    default:
+        break;
+    }
+  return 0;
+}
