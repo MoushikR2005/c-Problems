3ea2df7b339874f27792f369fb6fde0c85ddc014
Moushik R <moushikmoushik55@gmail.com>
2026-01-28 22:37:35 +0530

    Add files via upload

---
+++ forloopq11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    int sum =0;
+    scanf("%d",&n);
+  for(int i=0;i<=n;i++){
+    int digts = n%10;
+    sum +=digts;
+    n=n/10;
+  }
+
+printf("%d",sum);
+return 0;
+}
